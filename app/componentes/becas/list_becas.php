a4d3f40f33aa40d6776b56f770eca1a27ba25240
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-08-13 08:53:03 -0500

    Se agrega servicio de listar becas

---
+++ app/componentes/becas/list_becas.php
@@ -0,0 +1,21 @@
+<?php
+ header('Access-Control-Allow-Origin: *'); 
+ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+
+ 
+require_once "../../../bootstrap.php";
+
+$becas = $entityManager->createQueryBuilder()
+->select('b')  // select * 
+->from('Becas', 'b') // from becas u 
+->getQuery()
+->getArrayResult();
+
+if ($becas === null) {
+    echo "No hay becas found.\n";
+    exit(1);
+}
+
+echo json_encode($becas);
+header('Content-Type: application/json');
