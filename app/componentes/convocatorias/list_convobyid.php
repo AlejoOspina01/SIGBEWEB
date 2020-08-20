a120f63e6b3ab60c1b09e3614b8710fa3075e024
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2020-08-20 13:46:12 -0500

    Se agrega el listar convo por id

---
+++ app/componentes/convocatorias/list_convobyid.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+require_once "../../../bootstrap.php";
+
+$idConvo = $_GET["idconvo"];
+$convobuscada = $entityManager->createQuery('select c from Convocatorias c where c.consecutivo_convocatoria = ?1')
+->setParameter(1, $idConvo)
+->getSingleResult();
+
+if ($convobuscada === null) {
+    echo "No usuario found.\n";
+    exit(1);
+}
+
+ $convoarray =  array(
+      'fechainicio'     => $convobuscada->getFechaInicio()->format('Y-m-d H:i'),
+      'fechafin'         => $convobuscada->getFechaFin()->format('Y-m-d H:i'),
+      'estadoconvo'      => $convobuscada->getEstadoConvocatoria(),
+      'cupo'         => $convobuscada->getCupo(),
+      'beca'          => $convobuscada->getConsecutivoBeca()->getDescripcion(),
+      'periodo'     =>  array('fechaInicioperiodo' => $convobuscada->getConsecutivoPeriodo()->getFechaInicio()->format('Y-m-d H:i'),
+        'fechaFinperiodo' => $convobuscada->getConsecutivoPeriodo()->getFechaFin()->format('Y-m-d H:i')
+    )
+    );
+
+echo json_encode($convoarray);
+
+header('Content-Type: application/json');
