24610375a2311e7f3d53e54011f68396d8edfdb2
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2020-09-22 10:10:31 -0500

    Sse crea el servicio para actualizarEstado ticket

---
+++ app/componentes/Tickets/actualizarEstado.php
@@ -0,0 +1,19 @@
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  require_once "../../../bootstrap.php";
+  
+  $json = file_get_contents('php://input');
+ 
+  $params = json_decode($json);
+  
+  $stdTicket = get_object_vars($params);  
+  
+  $ticketUpdate = $entityManager->createQueryBuilder();
+$query = $ticketUpdate->update('Tickets', 't') 
+        ->set('t.estadoticket', '?2')
+        ->where('t.consecutivoticket = ?1')
+        ->setParameter(1,$params['codigoTicket'] )
+        ->setParameter(2,'Usado' )
+        ->getQuery();
+        $execute = $query->execute();
