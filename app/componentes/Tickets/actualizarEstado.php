c93c7751a9df683e3acea96af670ab6e6038973a
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2020-09-25 08:55:21 -0500

    Se correge errores en el archivo modifcarEstado

--- app/componentes/Tickets/actualizarEstado.php
+++ app/componentes/Tickets/actualizarEstado.php
@@ -8,12 +8,19 @@
   $params = json_decode($json);
   
   $stdTicket = get_object_vars($params);  
-  
+
+$buscarTicket = $entityManager->find('Tickets', $stdTicket['codigoTicket']);
+
+if(empty($buscarTicket->getEstado())){
+}else{
   $ticketUpdate = $entityManager->createQueryBuilder();
-$query = $ticketUpdate->update('Tickets', 't') 
-        ->set('t.estadoticket', '?2')
-        ->where('t.consecutivoticket = ?1')
-        ->setParameter(1,$params['codigoTicket'] )
-        ->setParameter(2,'Usado' )
-        ->getQuery();
-        $execute = $query->execute();
+    $query = $ticketUpdate->update('Tickets', 't') 
+            ->set('t.estadoticket', '?2')
+            ->where('t.consecutivoticket = ?1')
+            ->setParameter(1,$stdTicket['codigoTicket'] )
+            ->setParameter(2,'Usado' )
+            ->getQuery();
+            $execute = $query->execute();
+  $respuesta = array("resultado" => "Actualizado con exito");
+  echo json_encode($respuesta);
+}
