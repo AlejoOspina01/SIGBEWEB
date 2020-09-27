a80f0e94181c09b2770b5695389134dccfe02461
GustavoQuinteroVergara <quintero.gustavo@correounivalle.edu.co>
2020-09-27 15:35:55 -0500

    Se realiza cambio(NO ISSUE) en listar periodos(Solo mostrar ultimo) y se corrige ISSUE de convocatorias fecha

--- app/componentes/periodosacademicos/list_periodosacademicos.php
+++ app/componentes/periodosacademicos/list_periodosacademicos.php
@@ -1,5 +1,8 @@
 <?php
- header('Access-Control-Allow-Origin: *'); 
+
+use Doctrine\ORM\Mapping\OrderBy;
+
+header('Access-Control-Allow-Origin: *'); 
  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 
@@ -9,6 +12,8 @@ require_once "../../../bootstrap.php";
 $periodo = $entityManager->createQueryBuilder()
 ->select('pa.consecutivo_periodo, pa.descripcion')  
 ->from('Periodosacademicos', 'pa') 
+->OrderBy('pa.consecutivo_periodo', 'DESC')
+->setMaxResults(1)
 ->getQuery()
 ->getArrayResult();
 
