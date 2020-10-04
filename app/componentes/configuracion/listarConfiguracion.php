df9465e5fe4116f117eee000a8e2cd3ccecf4652
AlejoOspina01 <60979547+AlejoOspina01@users.noreply.github.com>
2020-10-03 23:56:45 -0500

    Update listarConfiguracion.php

--- app/componentes/configuracion/listarConfiguracion.php
+++ app/componentes/configuracion/listarConfiguracion.php
@@ -13,5 +13,13 @@ for ($i=0; $i < sizeof($textoArchivo); $i++) {
 for ($i=0; $i < sizeof($variables); $i++) { 
     $variables[$i][1] = trim($variables[$i][1]);
 }
+$arrayResultante= array(
+	"valorticketalmuerzo" => $variables[0][1],
+	"valorticketrefrigerio" => $variables[1][1],
+	"horainicioVentaAlmuerzo" => $variables[2][1],
+	"horaFinVentaAlmuerzo" => $variables[3][1],
+	"horainicioVentaRefrigerio" => $variables[4][1],
+	"horaFinVentaRefrigerio" => $variables[5][1]
+);
 
-echo json_encode($variables);
+echo json_encode($arrayResultante);
