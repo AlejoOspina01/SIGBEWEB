fa1f67ca792651c17a3b347ae986fa6f1acab5eb
AlejoOspina01 <alejandro.ospina@correounivalle.edu.co>
2021-02-23 15:16:19 -0500

    Se agrega servicio para consultarbeneficiario

---
+++ app/componentes/postulacion/postulacionbeneficario.php
@@ -0,0 +1,40 @@
+<?php
+
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+// show_roles.php <id>
+require_once "../../../bootstrap.php";
+
+$useriden = $_GET['iduser'];
+$tipoticket = $_GET['tipoticket'];
+
+$periodoencontrado = $entityManager->createQuery('SELECT pa FROM Periodosacademicos pa WHERE pa.fecha_fin = (SELECT MAX(p.fecha_fin) from Periodosacademicos p)')
+->getSingleResult();
+
+$convocatoriasEncontradas=null;
+
+if($tipoticket == 1){
+	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 1')
+	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
+	->getSingleResult();
+}else{
+	$convocatoriasEncontradas = $entityManager->createQuery('SELECT c FROM Convocatorias c WHERE c.periodosacademicos = ?1 AND c.becas = 2')
+	->setParameter(1, $periodoencontrado->getConsecutivo_periodo())
+	->getSingleResult();
+}
+
+$postulacionesEncontradas = $entityManager->createQuery('SELECT p FROM Postulacion p WHERE p.convocatoria = ?1 AND p.usuario = ?2 AND p.estado_postulacion = ?3')
+->setParameter(1, $convocatoriasEncontradas->getConsecutivoConvocatoria())
+->setParameter(2, $useriden)
+->setParameter(3, 'Aprobado')
+->getSingleResult();
+
+$postulacionfound =  array(
+	'consecutivo_postulacion'     => $postulacionesEncontradas->getConsecutivo_postulacion(),
+	'fecha'         => $postulacionesEncontradas->getFechapostulacion(),
+	'estrato'    => $postulacionesEncontradas->getEstrato(),
+);
+
+
+
+echo json_encode($postulacionfound);
