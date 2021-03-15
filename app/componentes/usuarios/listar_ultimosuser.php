3b4c73b0e0ae8b4fc5927e919dbba5f5712af8df
WilliamRueda <william.rueda@correounivalle.edu.co>
2021-03-15 14:21:50 -0500

    se crearon los servicios para crear el usuariocarrera y se actualizo el servicio de crear usuario

---
+++ app/componentes/usuarios/listar_ultimosuser.php
@@ -0,0 +1,20 @@
+<?php
+ header('Access-Control-Allow-Origin: *'); 
+ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+require_once "../../../bootstrap.php";
+
+$usuarios = $entityManager->createQuery("
+SELECT * 
+FROM usuarios u
+ORDER BY u.identificacion DESC
+LIMIT 5 
+"    
+);
+
+$ultuimosusuarios
+
+
+
+
+
